6944454b2e5d67f0d78262af719e0b36aeb9f05b
itolab <itolab@haag.itolab.nitech.ac.jp>
2013-10-22 01:03:48 +0900

    add install ruby envrionment to installer

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-
+git clone https://github.com/sstephenson/rbenv.git .rbenv
+cd $HOME
+mkdir .rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins/ruby-build
+git clone https://github.com/jf/rbenv-gemset.git .rbenv/plugins/rbenv-gemset
