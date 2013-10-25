2de23f6c8c7a80fd5fc0d87991ab8418913f9831
yshtak <yshtak@gmail.com>
2013-10-26 04:12:02 +0900

    tmuxの設定ファイル追加

--- dot.zshrc
+++ dot.zshrc
@@ -3,7 +3,8 @@ source ~/.zsh.d/zshrc
 
 export NEO4J_HOME="/opt/neo4j"
 export PATH="$HOME/.rbenv/bin:$PATH:$NEO4J_HOME/bin"
-export PATH="/opt/node/bin:$PATH:"
+export PATH="/opt/node-0.10.19/bin:$PATH:"
+#export PATH="/usr/local/node-0.10.19/bin:$PATH:"
 
 eval "$(rbenv init -)"
 
@@ -13,3 +14,19 @@ alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
 #alias sudo='sudo env PATH=$PATH'
+
+# tmux autostart configure
+if [ -z "$TMUX" -a -z "$STY" ]; then
+    if type tmuxx >/dev/null 2>&1; then
+        tmuxx
+    elif type tmux >/dev/null 2>&1; then
+        if tmux has-session && tmux list-sessions | egrep -q '.*]$'; then
+            # デタッチ済みセッションが存在する
+            tmux attach && echo "tmux attached session "
+        else
+            tmux -2 new-session && echo "tmux created new session"
+        fi
+    elif type screen >/dev/null 2>&1; then
+        screen -rx || screen -D -RR
+    fi
+fi
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 # Vimの初期設定
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+vim -c NeoBundleInstall
 
 # ruby configuration
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
@@ -15,9 +16,23 @@ cd $SCRIPT_DIR
 mkdir -p ~/.fonts
 cp $SCRIPT_DIR/fonts/Inconsolata+for+Powerline.otf ~/.fonts
 cp $SCRIPT_DIR/fonts/PowerlineSymbols.otf ~/.fonts
+git clone git@github.com:yascentur/Ricty.git ~/Ricty
+cd ~/Ricty
+wget http://levien.com/type/myfonts/Inconsolata.otf
+wget -O mig1m.zip 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fmix-mplus-ipa%2F57240%2Fmigu-1m-20121030.zip'
+unzip mig1m.zip
+rm mig1m.zip 
+sh ricty_generator.sh auto
+cp Ricty-Regular.ttf Ricty-Bold.ttf ~/.vim/bundle/powerline/font
+cd ~/.vim/bundle/powerline/font
+fontforge -lang=py -script fontpatcher.py Ricty-Regular.ttf
+fontforge -lang=py -script fontpatcher.py Ricty-Bold.ttf
+cp -f Ricty* ~/.fonts
+fc-cache -vf
 
 # dot Files
 cp dot.Xdefaults ~/.Xdefaults
 cp -r dot.zsh.d ~/.zsh.d
 cp dot.zshrc ~/.zshrc
 cp dot.vimrc ~/.vimrc
+cp dot.tmux.conf ~/.tmux.conf
