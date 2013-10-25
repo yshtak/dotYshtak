4bad7a4eb64e5bae249c693032fc076da6d8ea6a
yshtak <yshtak@gmail.com>
2013-10-25 20:18:52 +0900

    インストールスクリプトとurxvtの設定ファイル追加

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ### 自分用のDotファイル類のサンプル
 ※zshの設定ファイルは
 	[https://github.com/clear-code/zsh.d] (https://github.com/clear-code/zsh.d)の設定集を参考にしています。
+
+### 設定ファイルについて
+* zsh
+ > dot.zsh.d、dot.zshrcはzshの初期設定用のファイル
+* urxvt
+ > dot.Xdefaultsはurxvtの設定ファイル
+* vim
+ > dot.vimrc 
--- install.sh
+++ install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+# Vimの初期設定
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/sstephenson/rbenv.git .rbenv
-cd $HOME
-mkdir .rbenv/plugins
+
+# ruby configuration
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+mkdir ~/.rbenv/plugins
 git clone https://github.com/sstephenson/ruby-build.git .rbenv/plugins/ruby-build
 git clone https://github.com/jf/rbenv-gemset.git .rbenv/plugins/rbenv-gemset
+
+# Fonts Configuration
+cd $SCRIPT_DIR
+mkdir -p ~/.fonts
+cp $SCRIPT_DIR/fonts/Inconsolata+for+Powerline.otf ~/.fonts
+cp $SCRIPT_DIR/fonts/PowerlineSymbols.otf ~/.fonts
+
+# dot Files
+cp dot.Xdefaults ~/.Xdefaults
+cp -r dot.zsh.d ~/.zsh.d
+cp dot.zshrc ~/.zshrc
+cp dot.vimrc ~/.vimrc
